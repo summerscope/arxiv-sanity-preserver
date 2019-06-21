502ee01397fc0890e48c43b5398b02647479ac09
Laura Summers <summerscope@gmail.com>
2019-06-21 15:00:54 +1000

    Adding another arXiv category to the search query

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ source env/bin/activate || exit 1
 
 PYTHON="nice python -u"
 
-$PYTHON fetch_papers.py --search-query="%28cat:cs.CV+OR+cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.NE+OR+cat:stat.ML%29+AND+%28all:fairness+OR+all:ethical+all:ethics+all:safety%29" --max-index 2000 #--break-on-no-added 0
+$PYTHON fetch_papers.py --search-query="%28cat:cs.CV+OR+cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.NE+OR+cat:cs.CY+OR+cat:stat.ML%29+AND+%28all:fairness+OR+all:ethical+all:ethics+all:safety%29" --max-index 2000 #--break-on-no-added 0
 $PYTHON download_pdfs.py
 $PYTHON parse_pdf_to_text.py
 $PYTHON thumb_pdf.py
